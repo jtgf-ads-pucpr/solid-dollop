fbb928f63c551b52a061654ccb63e2ae4bab2f5e
jtgf-ads-pucpr <fernandes.jhonatan@pucpr.edu.br>
2026-08-08 14:59:54 -0300

    Update da main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():  
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, World!"} 
+
+@app.get("/teste1")
+async def funcao_teste():
+    return {"message": True, "num_aleatorio": random.randint(1, 1000)}
