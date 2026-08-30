9881683a1c803891ed21f5c6c3195218d8a20381
jhonatan_TGF <fernandes.jhonatan@pucpr.edu.br>
2026-08-30 19:34:55 -0300

    integracao-docker-github

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 import random
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Estudante(BaseModel):
+    nome: str
+    curso: str
+    ativo: bool
 
 @app.get("/helloworld")
 async def read_root():  
@@ -10,3 +17,15 @@ async def read_root():
 @app.get("/teste")
 async def funcaoteste():  
     return {"teste": True, "num_aleatorio": random.randint(1, 1000)}
+
+@app.post("/estudante/cadastro")
+async def cadastrar_estudante(estudante: Estudante):
+    return {"message": "Estudante cadastrado com sucesso!", "estudante": estudante}
+
+@app.put("/estudante/update/{id_estudante}")
+async def atualizar_estudante(id_estudante: int, estudante: Estudante):
+    return {"message": f"Estudante com ID {id_estudante} atualizado com sucesso!", "estudante": estudante}
+
+@app.delete("/estudante/delete/{id_estudante}")
+async def deletar_estudante(id_estudante: int):
+    return {"message": f"Estudante com ID {id_estudante} deletado com sucesso!"}
